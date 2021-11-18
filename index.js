c8f61c4d48c552ff4292d7acf4e6926d06c60e9a
Paulo Hernane <paulohernane10@gmail.com>
2021-11-18 14:53:13 -0300

    adicionando o CORS na API

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import todoRouter from './routes/todo.router.js';
 
@@ -7,6 +8,7 @@ const app = express();
 const port = 3333;
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/', todoRouter);
 
