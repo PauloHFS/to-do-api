463716ad2f22e2cad22c31185cde27e72944da4a
Paulo Hernane <paulohernane10@gmail.com>
2021-11-19 20:05:42 -0300

    update port config

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import todoRouter from './routes/todo.router.js';
 
 const app = express();
 
-const port = 3333;
+const port = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(cors());
@@ -13,5 +13,5 @@ app.use(cors());
 app.use('/', todoRouter);
 
 app.listen(port, () => {
-  console.log(`Running app at http://localhost:${port}`);
+  console.log(`Running app at https://to-do-api-pauloh.herokuapp.com:${port}`);
 });
